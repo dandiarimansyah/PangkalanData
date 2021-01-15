ecb376a03faed079863a6458f13d9362bd8c3795
dimas_andhika <dimasandhikaaa@gmail,com>
2021-01-15 15:33:07 +0700

    Alternatif Login

---
+++ PangkalanData/resources/views/VALIDATOR/login2.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"> </script>
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vlogin2.css" />
+    <title>Halaman Masuk</title>
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form action="#" class="sign-in-form">
+            <h2 class="title">Masuk Validator / Operator</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" placeholder="Nama Pengguna" />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Kata Sandi" />
+            </div>
+            <input type="submit" value="Masuk" class="btn solid" />
+
+
+            <a href="#" class="kembali"> 
+                <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                Kembali ke Laman Utama
+              </a>
+
+            <!-- <input class="kembali" type="submit" value="Kembali ke Laman Utama" class="btn solid" /> -->
+          </form>
+         
+          
+
+        </div>
+      </div>
+
+      <div class="panels-container">
+        <div class="panel left-panel">
+          <img src="img/validator.svg" class="image"/>
+        </div>
+
+        
+    </div>
+
+    <script src="app.js"></script>
+  </body>
+</html>
