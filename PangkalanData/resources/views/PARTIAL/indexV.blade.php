ce8e35c0177f8a74c18c603aef60e2bd81b29a61
Dandi Arimansyah <dandimansyah@gmail.com>
2021-01-22 22:54:52 +0700

    Menambah Input & Edit Controller

--- PangkalanData/resources/views/PARTIAL/indexV.blade.php
+++ PangkalanData/resources/views/PARTIAL/indexV.blade.php
@@ -15,7 +15,6 @@
             <link rel="stylesheet" type="text/css" href="{{ asset('stylecss/guest.css') }}">
         @endguest
         
-
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
         <script src="https://kit.fontawesome.com/a076d05399.js"></script>
