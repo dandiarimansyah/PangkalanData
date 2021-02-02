2ca03d2a29daaecca15b3f43f3cd84bc8d72cce8
Dandi Arimansyah <dandimansyah@gmail.com>
2021-02-02 22:10:50 +0700

    Fix Migration Penelitian

--- PangkalanData/database/migrations/2021_02_02_150426_create_penelitian_table.php
+++ PangkalanData/database/migrations/2021_02_02_150426_create_penelitian_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDataPenelitianTable extends Migration
+class CreatePenelitianTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,19 +13,20 @@ class CreateDataPenelitianTable extends Migration
      */
     public function up()
     {
-        Schema::create('data_penelitian', function (Blueprint $table) {
+        Schema::create('penelitian', function (Blueprint $table) {
             $table->id();
-            $table->string("kategori_penelitian");
-            $table->string("unit/satuan_kerja");
+            $table->string("kategori");
+            $table->string("unit");
             $table->string("peneliti");
-            $table->longText("judul");
+            $table->string("judul");
             $table->string("kerja_sama")->nullable();
-            $table->date("tanggal_mulai_penelitian")->nullable();
-            $table->date("tanggal_selesai_penelitian")->nullable();
+            $table->string("tanggal_awal")->nullable();
+            $table->string("tanggal_akhir")->nullable();
             $table->string("lama_penelitian")->nullable();
             $table->string("publikasi")->nullable();
             $table->string("tahun_terbit")->nullable();
-            $table->longText("abstrak");
+            $table->string("abstrak");
+            $table->string("file")->nullable();
             $table->timestamps();
         });
     }
@@ -37,6 +38,6 @@ class CreateDataPenelitianTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('data_penelitian');
+        Schema::dropIfExists('penelitian');
     }
 }
