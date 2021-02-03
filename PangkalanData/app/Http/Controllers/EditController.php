542b0909ca88a4338ba7bfebe69aee81bfed6769
Dandi Arimansyah <dandimansyah@gmail.com>
2021-02-03 21:41:02 +0700

    Edit Controller Pre-Progress

--- PangkalanData/app/Http/Controllers/EditController.php
+++ PangkalanData/app/Http/Controllers/EditController.php
@@ -3,13 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Anggaran;
+use App\Models\Bengkel_Sastra_Dan_Bahasa;
+use App\Models\Duta_Nasional;
+use App\Models\Duta_Provinsi;
+use App\Models\Inventarisasi;
+use App\Models\Jurnal;
+use App\Models\Kamus;
+use App\Models\Realisasi_Anggaran;
+use App\Models\Kepegawaian;
+use App\Models\Kerja_Sama;
+use App\Models\Komunitas_Bahasa;
+use App\Models\Komunitas_Sastra;
+use App\Models\Musikalisasi_Puisi_Nasional;
+use App\Models\Musikalisasi_Puisi_Provinsi;
+use App\Models\Penelitian;
+use App\Models\Penghargaan_Bahasa;
+use App\Models\Penghargaan_Sastra;
+use App\Models\Penyuluhan;
+use App\Models\Perpustakaan;
+use App\Models\Pesuluh;
+use App\Models\Tanah_Bangunan;
+use App\Models\Terbitan_Umum;
 
 class EditController extends Controller
 {
     //EDIT KATEGORI A
     public function a1()
     {
-        return view('EDIT.SEKRETARIAT.a1');
+        $anggaran = Anggaran::all();
+
+        return view('EDIT.SEKRETARIAT.a1', compact('anggaran'));
     }
     public function a2()
     {
