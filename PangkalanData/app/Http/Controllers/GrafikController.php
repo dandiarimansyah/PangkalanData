77689f5afb79e402827ae9881e690f839dad0c6f
dimas_andhika <dimasandhikaaa@gmail,com>
2021-02-16 15:36:27 +0700

    Grafik

--- PangkalanData/app/Http/Controllers/GrafikController.php
+++ PangkalanData/app/Http/Controllers/GrafikController.php
@@ -2,15 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Grafik;
+
 use Illuminate\Http\Request;
+
+use App\Models\Anggaran;
+use App\Models\Bengkel_Sastra_Dan_Bahasa;
+use App\Models\Duta_Nasional;
+use App\Models\Duta_Provinsi;
+use App\Models\Inventarisasi;
+use App\Models\Jurnal;
+use App\Models\Kamus;
+use App\Models\Realisasi_Anggaran;
+use App\Models\Kepegawaian;
+use App\Models\Kerja_Sama;
+use App\Models\Komunitas_Bahasa;
+use App\Models\Komunitas_Sastra;
+use App\Models\Musikalisasi_Puisi_Nasional;
+use App\Models\Musikalisasi_Puisi_Provinsi;
+use App\Models\Penelitian;
+use App\Models\Penghargaan_Bahasa;
+use App\Models\Penghargaan_Sastra;
+use App\Models\Penyuluhan;
+use App\Models\Perpustakaan;
+use App\Models\Pesuluh;
+use App\Models\Tanah_Bangunan;
+use App\Models\Terbitan_Umum;
 
 class GrafikController extends Controller
 {
     //GRAFIK S 1
     public function ga1()
     {
-        return view('GRAFIK.SEKRETARIAT.ga1');
+        $anggaran = Anggaran::all();
+        // $tahun_angaran = \App\
+
+        // menyiapkan data untuk chart
+        $categories = [];
+
+        foreach($anggaran as $ta){
+            $categories[] = $ta->nama;
+        }
+
+        return view('GRAFIK.SEKRETARIAT.ga1', ['categories' => $categories]);
     }
+
     public function ga2()
     {
         return view('GRAFIK.SEKRETARIAT.ga2');
