5cd94943b59d1e88f3b4e51d9aafcb222ef4cdb5
Dandi Arimansyah <dandimansyah@gmail.com>
2021-02-18 19:20:26 +0700

    Revisi

--- PangkalanData/app/Http/Controllers/OperatorController.php
+++ PangkalanData/app/Http/Controllers/OperatorController.php
@@ -3,6 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Anggaran;
+use App\Models\Bengkel_Sastra_Dan_Bahasa;
+use App\Models\Duta_Nasional;
+use App\Models\Duta_Provinsi;
+use App\Models\Inventarisasi;
+use App\Models\Jurnal;
+use App\Models\Kamus;
+use App\Models\Realisasi_Anggaran;
+use App\Models\Kepegawaian;
+use App\Models\Kerja_Sama;
+use App\Models\Komunitas_Bahasa;
+use App\Models\Komunitas_Sastra;
+use App\Models\Musikalisasi_Puisi_Nasional;
+use App\Models\Musikalisasi_Puisi_Provinsi;
+use App\Models\Penelitian;
+use App\Models\Penghargaan_Bahasa;
+use App\Models\Penghargaan_Sastra;
+use App\Models\Penyuluhan;
+use App\Models\Perpustakaan;
+use App\Models\Pesuluh;
+use App\Models\Tanah_Bangunan;
+use App\Models\Terbitan_Umum;
 
 class OperatorController extends Controller
 {
