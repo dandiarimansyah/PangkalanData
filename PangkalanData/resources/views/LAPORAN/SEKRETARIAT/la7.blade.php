747368b0835d082b436daf0523386af78355017d
Dandi Arimansyah <dandimansyah@gmail.com>
2021-02-18 12:08:38 +0700

    Beranda

--- PangkalanData/resources/views/LAPORAN/SEKRETARIAT/la7.blade.php
+++ PangkalanData/resources/views/LAPORAN/SEKRETARIAT/la7.blade.php
@@ -64,7 +64,7 @@
 
             <tbody>
 
-                @forelse ($inventarisasi as $key => $a)
+                @forelse ($data as $key => $a)
                     <tr>
                         <td>{{ $key + 1}}</td>
                         <td>{{ $a -> updated_at->format('m-d-Y')}}</td>
--- PangkalanData/resources/views/beranda.blade.php
+++ PangkalanData/resources/views/beranda.blade.php
@@ -2,8 +2,57 @@
 
 @section('content')
 
-<div class="content">
-    <header>HALAMAN BERANDA</header>
+<div class="judul_beranda">
+    <p>BALAI BAHASA JAWA TENGAH</p>
 </div>
-    
+
+<div class="gambar_beranda">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+          <img class="gambar_carousel" src="{{ asset('Gambar/balai.jpg')}}" alt="First slide">
+          <div class="carousel-caption d-none d-md-block caption">
+            <h5>Caption</h5>
+            <p>Caption</p>
+          </div>
+        </div>
+      <div class="carousel-item">
+          <img class="gambar_carousel" src="{{ asset('Gambar/balai2.jpg')}}" alt="Second slide">
+          <div class="carousel-caption d-none d-md-block caption">
+            <h5>Caption</h5>
+            <p>Caption</p>
+          </div>
+        </div>
+      <div class="carousel-item">
+        <img class="gambar_carousel" src="{{ asset('Gambar/balai3.jpg')}}" alt="Third slide">
+        <div class="carousel-caption d-none d-md-block caption">
+            <h5>Caption</h5>
+            <p>Caption</p>
+          </div>
+    </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  
+</div>
+
 @endsection
+
+@push('scripts')
+    <script>
+        $('.carousel').carousel()
+    </script>
+
+@endpush
