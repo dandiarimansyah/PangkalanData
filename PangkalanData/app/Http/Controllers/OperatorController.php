9959fd78344ef8d7e97c61e3c81a1c44ead33d2c
Dandi Arimansyah <dandimansyah@gmail.com>
2021-02-18 19:14:56 +0700

    Pre Data Tampil

--- PangkalanData/app/Http/Controllers/OperatorController.php
+++ PangkalanData/app/Http/Controllers/OperatorController.php
@@ -8,11 +8,293 @@ class OperatorController extends Controller
 {
     public function input()
     {
-        return view('OPERATOR.input');
+        $Anggaran = Anggaran::all()->count();
+        $Realisasi_Anggaran = Realisasi_Anggaran::all()->count();
+        $Kepegawaian = Kepegawaian::all()->count();
+        $Kerja_Sama = Kerja_Sama::all()->count();
+        $Tanah_Bangunan = Tanah_Bangunan::all()->count();
+        $Perpustakaan = Perpustakaan::all()->count();
+        $Inventarisasi = Inventarisasi::all()->count();
+        $Kamus = Kamus::all()->count();
+        $Jurnal = Jurnal::all()->count();
+        $Terbitan_Umum = Terbitan_Umum::all()->count();
+        $Penyuluhan = Penyuluhan::all()->count();
+        $Pesuluh = Pesuluh::all()->count();
+        $Penghargaan_Bahasa = Penghargaan_Bahasa::all()->count();
+        $Duta_Nasional = Duta_Nasional::all()->count();
+        $Duta_Provinsi = Duta_Provinsi::all()->count();
+        $Bengkel_Sastra_Dan_Bahasa = Bengkel_Sastra_Dan_Bahasa::all()->count();
+        $Penghargaan_Sastra = Penghargaan_Sastra::all()->count();
+        $Musikalisasi_Puisi_Nasional = Musikalisasi_Puisi_Nasional::all()->count();
+        $Musikalisasi_Puisi_Provinsi = Musikalisasi_Puisi_Provinsi::all()->count();
+        $Komunitas_Bahasa = Komunitas_Bahasa::all()->count();
+        $Komunitas_Sastra = Komunitas_Sastra::all()->count();
+        $Penelitian = Penelitian::all()->count();
+
+        $Anggaran_V = Anggaran::where('validasi', 'sudah')->get()->count();
+        $Realisasi_Anggaran_V = Realisasi_Anggaran::where('validasi', 'sudah')->get()->count();
+        $Kepegawaian_V = Kepegawaian::where('validasi', 'sudah')->get()->count();
+        $Kerja_Sama_V = Kerja_Sama::where('validasi', 'sudah')->get()->count();
+        $Tanah_Bangunan_V = Tanah_Bangunan::where('validasi', 'sudah')->get()->count();
+        $Perpustakaan_V = Perpustakaan::where('validasi', 'sudah')->get()->count();
+        $Inventarisasi_V = Inventarisasi::where('validasi', 'sudah')->get()->count();
+        $Kamus_V = Kamus::where('validasi', 'sudah')->get()->count();
+        $Jurnal_V = Jurnal::where('validasi', 'sudah')->get()->count();
+        $Terbitan_Umum_V = Terbitan_Umum::where('validasi', 'sudah')->get()->count();
+        $Penyuluhan_V = Penyuluhan::where('validasi', 'sudah')->get()->count();
+        $Pesuluh_V = Pesuluh::where('validasi', 'sudah')->get()->count();
+        $Penghargaan_Bahasa_V = Penghargaan_Bahasa::where('validasi', 'sudah')->get()->count();
+        $Duta_Nasional_V = Duta_Nasional::where('validasi', 'sudah')->get()->count();
+        $Duta_Provinsi_V = Duta_Provinsi::where('validasi', 'sudah')->get()->count();
+        $Bengkel_Sastra_Dan_Bahasa_V = Bengkel_Sastra_Dan_Bahasa::where('validasi', 'sudah')->get()->count();
+        $Penghargaan_Sastra_V = Penghargaan_Sastra::where('validasi', 'sudah')->get()->count();
+        $Musikalisasi_Puisi_Nasional_V = Musikalisasi_Puisi_Nasional::where('validasi', 'sudah')->get()->count();
+        $Musikalisasi_Puisi_Provinsi_V = Musikalisasi_Puisi_Provinsi::where('validasi', 'sudah')->get()->count();
+        $Komunitas_Bahasa_V = Komunitas_Bahasa::where('validasi', 'sudah')->get()->count();
+        $Komunitas_Sastra_V = Komunitas_Sastra::where('validasi', 'sudah')->get()->count();
+        $Penelitian_V = Penelitian::where('validasi', 'sudah')->get()->count();
+
+        $Total = $Anggaran +
+            $Realisasi_Anggaran +
+            $Kepegawaian +
+            $Kerja_Sama +
+            $Tanah_Bangunan +
+            $Perpustakaan +
+            $Inventarisasi +
+            $Kamus +
+            $Jurnal +
+            $Terbitan_Umum +
+            $Penyuluhan +
+            $Pesuluh +
+            $Penghargaan_Bahasa +
+            $Duta_Nasional +
+            $Duta_Provinsi +
+            $Bengkel_Sastra_Dan_Bahasa +
+            $Penghargaan_Sastra +
+            $Musikalisasi_Puisi_Nasional +
+            $Musikalisasi_Puisi_Provinsi +
+            $Komunitas_Bahasa +
+            $Komunitas_Sastra +
+            $Penelitian;
+
+        $Total_V = $Anggaran_V +
+            $Realisasi_Anggaran_V +
+            $Kepegawaian_V +
+            $Kerja_Sama_V +
+            $Tanah_Bangunan_V +
+            $Perpustakaan_V +
+            $Inventarisasi_V +
+            $Kamus_V +
+            $Jurnal_V +
+            $Terbitan_Umum_V +
+            $Penyuluhan_V +
+            $Pesuluh_V +
+            $Penghargaan_Bahasa_V +
+            $Duta_Nasional_V +
+            $Duta_Provinsi_V +
+            $Bengkel_Sastra_Dan_Bahasa_V +
+            $Penghargaan_Sastra_V +
+            $Musikalisasi_Puisi_Nasional_V +
+            $Musikalisasi_Puisi_Provinsi_V +
+            $Komunitas_Bahasa_V +
+            $Komunitas_Sastra_V +
+            $Penelitian_V;
+
+        return view('OPERATOR.input', compact(
+            'Anggaran',
+            'Realisasi_Anggaran',
+            'Kepegawaian',
+            'Kerja_Sama',
+            'Tanah_Bangunan',
+            'Perpustakaan',
+            'Inventarisasi',
+            'Kamus',
+            'Jurnal',
+            'Terbitan_Umum',
+            'Penyuluhan',
+            'Pesuluh',
+            'Penghargaan_Bahasa',
+            'Duta_Nasional',
+            'Duta_Provinsi',
+            'Bengkel_Sastra_Dan_Bahasa',
+            'Penghargaan_Sastra',
+            'Musikalisasi_Puisi_Nasional',
+            'Musikalisasi_Puisi_Provinsi',
+            'Komunitas_Bahasa',
+            'Komunitas_Sastra',
+            'Penelitian',
+
+            'Anggaran_V',
+            'Realisasi_Anggaran_V',
+            'Kepegawaian_V',
+            'Kerja_Sama_V',
+            'Tanah_Bangunan_V',
+            'Perpustakaan_V',
+            'Inventarisasi_V',
+            'Kamus_V',
+            'Jurnal_V',
+            'Terbitan_Umum_V',
+            'Penyuluhan_V',
+            'Pesuluh_V',
+            'Penghargaan_Bahasa_V',
+            'Duta_Nasional_V',
+            'Duta_Provinsi_V',
+            'Bengkel_Sastra_Dan_Bahasa_V',
+            'Penghargaan_Sastra_V',
+            'Musikalisasi_Puisi_Nasional_V',
+            'Musikalisasi_Puisi_Provinsi_V',
+            'Komunitas_Bahasa_V',
+            'Komunitas_Sastra_V',
+            'Penelitian_V',
+
+            'Total',
+            'Total_V',
+        ));
     }
 
     public function edit()
     {
-        return view('OPERATOR.edit');
+        $Anggaran = Anggaran::all()->count();
+        $Realisasi_Anggaran = Realisasi_Anggaran::all()->count();
+        $Kepegawaian = Kepegawaian::all()->count();
+        $Kerja_Sama = Kerja_Sama::all()->count();
+        $Tanah_Bangunan = Tanah_Bangunan::all()->count();
+        $Perpustakaan = Perpustakaan::all()->count();
+        $Inventarisasi = Inventarisasi::all()->count();
+        $Kamus = Kamus::all()->count();
+        $Jurnal = Jurnal::all()->count();
+        $Terbitan_Umum = Terbitan_Umum::all()->count();
+        $Penyuluhan = Penyuluhan::all()->count();
+        $Pesuluh = Pesuluh::all()->count();
+        $Penghargaan_Bahasa = Penghargaan_Bahasa::all()->count();
+        $Duta_Nasional = Duta_Nasional::all()->count();
+        $Duta_Provinsi = Duta_Provinsi::all()->count();
+        $Bengkel_Sastra_Dan_Bahasa = Bengkel_Sastra_Dan_Bahasa::all()->count();
+        $Penghargaan_Sastra = Penghargaan_Sastra::all()->count();
+        $Musikalisasi_Puisi_Nasional = Musikalisasi_Puisi_Nasional::all()->count();
+        $Musikalisasi_Puisi_Provinsi = Musikalisasi_Puisi_Provinsi::all()->count();
+        $Komunitas_Bahasa = Komunitas_Bahasa::all()->count();
+        $Komunitas_Sastra = Komunitas_Sastra::all()->count();
+        $Penelitian = Penelitian::all()->count();
+
+        $Anggaran_V = Anggaran::where('validasi', 'sudah')->get()->count();
+        $Realisasi_Anggaran_V = Realisasi_Anggaran::where('validasi', 'sudah')->get()->count();
+        $Kepegawaian_V = Kepegawaian::where('validasi', 'sudah')->get()->count();
+        $Kerja_Sama_V = Kerja_Sama::where('validasi', 'sudah')->get()->count();
+        $Tanah_Bangunan_V = Tanah_Bangunan::where('validasi', 'sudah')->get()->count();
+        $Perpustakaan_V = Perpustakaan::where('validasi', 'sudah')->get()->count();
+        $Inventarisasi_V = Inventarisasi::where('validasi', 'sudah')->get()->count();
+        $Kamus_V = Kamus::where('validasi', 'sudah')->get()->count();
+        $Jurnal_V = Jurnal::where('validasi', 'sudah')->get()->count();
+        $Terbitan_Umum_V = Terbitan_Umum::where('validasi', 'sudah')->get()->count();
+        $Penyuluhan_V = Penyuluhan::where('validasi', 'sudah')->get()->count();
+        $Pesuluh_V = Pesuluh::where('validasi', 'sudah')->get()->count();
+        $Penghargaan_Bahasa_V = Penghargaan_Bahasa::where('validasi', 'sudah')->get()->count();
+        $Duta_Nasional_V = Duta_Nasional::where('validasi', 'sudah')->get()->count();
+        $Duta_Provinsi_V = Duta_Provinsi::where('validasi', 'sudah')->get()->count();
+        $Bengkel_Sastra_Dan_Bahasa_V = Bengkel_Sastra_Dan_Bahasa::where('validasi', 'sudah')->get()->count();
+        $Penghargaan_Sastra_V = Penghargaan_Sastra::where('validasi', 'sudah')->get()->count();
+        $Musikalisasi_Puisi_Nasional_V = Musikalisasi_Puisi_Nasional::where('validasi', 'sudah')->get()->count();
+        $Musikalisasi_Puisi_Provinsi_V = Musikalisasi_Puisi_Provinsi::where('validasi', 'sudah')->get()->count();
+        $Komunitas_Bahasa_V = Komunitas_Bahasa::where('validasi', 'sudah')->get()->count();
+        $Komunitas_Sastra_V = Komunitas_Sastra::where('validasi', 'sudah')->get()->count();
+        $Penelitian_V = Penelitian::where('validasi', 'sudah')->get()->count();
+
+        $Total = $Anggaran +
+            $Realisasi_Anggaran +
+            $Kepegawaian +
+            $Kerja_Sama +
+            $Tanah_Bangunan +
+            $Perpustakaan +
+            $Inventarisasi +
+            $Kamus +
+            $Jurnal +
+            $Terbitan_Umum +
+            $Penyuluhan +
+            $Pesuluh +
+            $Penghargaan_Bahasa +
+            $Duta_Nasional +
+            $Duta_Provinsi +
+            $Bengkel_Sastra_Dan_Bahasa +
+            $Penghargaan_Sastra +
+            $Musikalisasi_Puisi_Nasional +
+            $Musikalisasi_Puisi_Provinsi +
+            $Komunitas_Bahasa +
+            $Komunitas_Sastra +
+            $Penelitian;
+
+        $Total_V = $Anggaran_V +
+            $Realisasi_Anggaran_V +
+            $Kepegawaian_V +
+            $Kerja_Sama_V +
+            $Tanah_Bangunan_V +
+            $Perpustakaan_V +
+            $Inventarisasi_V +
+            $Kamus_V +
+            $Jurnal_V +
+            $Terbitan_Umum_V +
+            $Penyuluhan_V +
+            $Pesuluh_V +
+            $Penghargaan_Bahasa_V +
+            $Duta_Nasional_V +
+            $Duta_Provinsi_V +
+            $Bengkel_Sastra_Dan_Bahasa_V +
+            $Penghargaan_Sastra_V +
+            $Musikalisasi_Puisi_Nasional_V +
+            $Musikalisasi_Puisi_Provinsi_V +
+            $Komunitas_Bahasa_V +
+            $Komunitas_Sastra_V +
+            $Penelitian_V;
+
+        return view('OPERATOR.edit', compact(
+            'Anggaran',
+            'Realisasi_Anggaran',
+            'Kepegawaian',
+            'Kerja_Sama',
+            'Tanah_Bangunan',
+            'Perpustakaan',
+            'Inventarisasi',
+            'Kamus',
+            'Jurnal',
+            'Terbitan_Umum',
+            'Penyuluhan',
+            'Pesuluh',
+            'Penghargaan_Bahasa',
+            'Duta_Nasional',
+            'Duta_Provinsi',
+            'Bengkel_Sastra_Dan_Bahasa',
+            'Penghargaan_Sastra',
+            'Musikalisasi_Puisi_Nasional',
+            'Musikalisasi_Puisi_Provinsi',
+            'Komunitas_Bahasa',
+            'Komunitas_Sastra',
+            'Penelitian',
+
+            'Anggaran_V',
+            'Realisasi_Anggaran_V',
+            'Kepegawaian_V',
+            'Kerja_Sama_V',
+            'Tanah_Bangunan_V',
+            'Perpustakaan_V',
+            'Inventarisasi_V',
+            'Kamus_V',
+            'Jurnal_V',
+            'Terbitan_Umum_V',
+            'Penyuluhan_V',
+            'Pesuluh_V',
+            'Penghargaan_Bahasa_V',
+            'Duta_Nasional_V',
+            'Duta_Provinsi_V',
+            'Bengkel_Sastra_Dan_Bahasa_V',
+            'Penghargaan_Sastra_V',
+            'Musikalisasi_Puisi_Nasional_V',
+            'Musikalisasi_Puisi_Provinsi_V',
+            'Komunitas_Bahasa_V',
+            'Komunitas_Sastra_V',
+            'Penelitian_V',
+
+            'Total',
+            'Total_V',
+        ));
     }
 }
--- PangkalanData/resources/views/GUEST/g_index.blade.php
+++ PangkalanData/resources/views/GUEST/g_index.blade.php
@@ -4,8 +4,226 @@
 
 @include('PARTIAL.MenuData')
 
-    <div class="content">
-        <header>HALAMAN DEPAN</header>
+<div class="isi-konten">
+
+    <div class="judul">
+        <th>DATA BALAI BAHASA JAWA TENGAH</th>
     </div>
+
+    <!-- TABLE -->
+    <div class="validasi">
+        <table class="content-table kanan">
+            <thead>
+                <tr>
+                    <th colspan="3">Data yang belum tervalidasi</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td style="font-weight:bold">Sekretariat</td>
+                    <td style="text-align:left">Tanah dan Bangunan</td>
+                    <td>5</td>
+                </tr>
+                <tr>
+                    <td style="font-weight:bold">Data Kebahasaan</td>
+                    <td style="text-align:left">Penyuluhan</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td style="font-weight:bold">Data Kesastraan</td>
+                    <td style="text-align:left">Penghargaan Sastra</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td style="font-weight:bold">Data Komunitas</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td style="font-weight:bold">Data Penelitian</td>
+                    <td style="text-align:left">Penelitian</td>
+                    <td>8</td>
+
+                </tr>
+            </tbody>
+        </table>
+
+        <!-- TABLE -->
+        <table class="content-table kanan">
+            <thead>
+                <tr>
+                    <th>Kategori</th>
+                    <th>Jenis Data</th>
+                    <th>Data Terinput</th>
+                    <th>Data Tervalidasi</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td style="font-weight:bold">Sekretariat</td>
+                    <td style="text-align:left">Anggaran</td>
+                    <td>{{$Anggaran}}</td>
+                    <td>{{$Anggaran_V}}</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Realisasi Anggaran</td>
+                    <td>{{$Realisasi_Anggaran}}</td>
+                    <td>{{$Realisasi_Anggaran_V}}</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Kepegawaian</td>
+                    <td>{{$Kepegawaian}}</td>
+                    <td>{{$Kepegawaian_V}}</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Kerja Sama</td>
+                    <td>127</td>
+                    <td>68</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Tanah Bangunan</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Perpustakaan</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Inventarisasi BMN</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+
+                <tr>
+                    <td style="font-weight:bold">Data Kebahasaan</td>
+                    <td style="text-align:left">Kamus/Ensiklopedia</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Jurnal/Majalah</td>
+                    <td>11</td>
+                    <td>11</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Terbitan Umum</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Penyuluhan</td>
+                    <td>127</td>
+                    <td>68</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Pesuluh</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Penghargaan Bahasa</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Duta Bahasa Nasional</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Duta Bahasa Provinsi</td>
+                    <td>5</td>
+                    <td>5</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td style="font-weight:bold">143</td>
+                    <td style="font-weight:bold">84</td>
+                </tr>
+
+                <tr>
+                    <td style="font-weight:bold">Data Kesastraan</td>
+                    <td style="text-align:left">Bengkel Satra</td>
+                    <td>23</td>
+                    <td>23</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Penghargaan Satra</td>
+                    <td>3</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Musikalisasi Puisi Nasional</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td style="text-align:left">Musikalisasi Puisi Provinsi</td>
+                    <td>4</td>
+                    <td>4</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td style="font-weight:bold">30</td>
+                    <td style="font-weight:bold">27</td>
+                </tr>
+            
+                <tr>
+                    <td style="font-weight:bold">Data Komunitas</td>
+                    <td style="text-align:left">Komunitas Bahasa dan Sastra</td>
+                    <td>392</td>
+                    <td>392</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td style="font-weight:bold">392</td>
+                    <td style="font-weight:bold">392</td>
+                </tr>
+
+                <tr>
+                    <td style="font-weight:bold">Data Penelitian</td>
+                    <td style="text-align:left">Penelitian</td>
+                    <td>255</td>
+                    <td>247</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td style="font-weight:bold">255</td>
+                    <td style="font-weight:bold">247</td>
+                </tr>
+
+                <tr class="total">
+                    <td colspan="2">TOTAL</td>
+                    <td>{{$Total}}</td>
+                    <td>{{$Total_V}}</td>
+                </tr>
+
+        </table>
+    </div>
+</div>
 
 @endsection
