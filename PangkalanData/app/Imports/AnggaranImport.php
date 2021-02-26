78eb7e34be6c5b6e1c608c1af4e0dce646b711ef
Dandi Arimansyah <dandimansyah@gmail.com>
2021-02-26 17:20:22 +0700

    Pre Revisi Template

--- PangkalanData/app/Imports/AnggaranImport.php
+++ PangkalanData/app/Imports/AnggaranImport.php
@@ -3,22 +3,28 @@
 namespace App\Imports;
 
 use App\Models\Anggaran;
-use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+// use Maatwebsite\Excel\Concerns\ToModel;
+// use Maatwebsite\Excel\Concerns\WithHeadingRow;
+// use Maatwebsite\Excel\Concerns\WithMappedCells;
 
-class AnggaranImport implements ToModel, WithHeadingRow
+class AnggaranImport implements ToCollection
 {
-    /**
-     * @param array $row
-     *
-     * @return \Illuminate\Database\Eloquent\Model|null
-     */
-    public function model(array $row)
+    public function collection(Collection $collection)
     {
-        return new Anggaran([
-            'unit' => 'Balai Bahasa Provinsi Jawa Tengah',
-            'tahun_anggaran' => $row['tahun_anggaran'],
-            'nilai_anggaran' => $row['nilai_anggaran'],
-        ]);
+        foreach ($collection as $key => $row) {
+            if ($key >= 3) {
+                // $tanggal = ($row[6] - 25569) * 86400;
+                // $tanggal2 = gmdate('Y-m-d', $tanggal);
+                // dd($tanggal2);
+
+                Anggaran::create([
+                    'unit' => 'Balai Bahasa Provinsi Jawa Tengah',
+                    'tahun_anggaran' => $row[1],
+                    'nilai_anggaran' => $row[2],
+                ]);
+            }
+        }
     }
 }
