dd97f7be90d99b8b69dc43b8d0a2d6b343fef7e1
Dandi Arimansyah <dandimansyah@gmail.com>
2021-02-27 15:35:50 +0700

    Pre Revisi Excel

--- PangkalanData/app/Imports/AnggaranImport.php
+++ PangkalanData/app/Imports/AnggaranImport.php
@@ -11,7 +11,7 @@ class AnggaranImport implements ToCollection
     public function collection(Collection $collection)
     {
         foreach ($collection as $key => $row) {
-            if ($key >= 1) {
+            if ($key >= 2) {
 
                 // $tgl = ($row[6] - 25569) * 86400;
                 // $tanggal = gmdate('Y-m-d', $tgl);
@@ -21,7 +21,6 @@ class AnggaranImport implements ToCollection
                 // dd($tanggal2);
 
                 Anggaran::create([
-                    'unit' => 'Balai Bahasa Provinsi Jawa Tengah',
                     'tahun_anggaran' => $row[3],
                     'nilai_anggaran' => $row[4],
                 ]);
--- PangkalanData/app/Imports/Bengkel_Sastra_Dan_BahasaImport.php
+++ PangkalanData/app/Imports/Bengkel_Sastra_Dan_BahasaImport.php
@@ -11,27 +11,26 @@ class Bengkel_Sastra_Dan_BahasaImport implements ToCollection
     public function collection(Collection $collection)
     {
         foreach ($collection as $key => $row) {
-            if ($key >= 1) {
+            if ($key >= 2) {
 
-                $tgl = ($row[6] - 25569) * 86400;
+                $tgl = ($row[5] - 25569) * 86400;
                 $tanggal = gmdate('Y-m-d', $tgl);
 
-                $tgl2 = ($row[7] - 25569) * 86400;
+                $tgl2 = ($row[6] - 25569) * 86400;
                 $tanggal2 = gmdate('Y-m-d', $tgl2);
                 // dd($tanggal2);
 
                 Bengkel_Sastra_Dan_Bahasa::create([
-                    'provinsi' => $row[3],
-                    'kota' => $row[4],
-                    'nama_kegiatan' => $row[5],
+                    'kota' => $row[3],
+                    'nama_kegiatan' => $row[4],
                     'tanggal_awal_pelaksanaan' => $tanggal,
                     'tanggal_akhir_pelaksanaan' => $tanggal2,
-                    'pemateri' => $row[8],
-                    'jumlah_peserta' => $row[9],
-                    'jumlah_sekolah' => $row[10],
-                    'jumlah_sekolah_yang_dibina' => $row[11],
-                    'nama_sekolah_yang_dibina' => $row[12],
-                    'aktivitas' => $row[13],
+                    'pemateri' => $row[7],
+                    'jumlah_peserta' => $row[8],
+                    'jumlah_sekolah' => $row[9],
+                    'jumlah_sekolah_yang_dibina' => $row[10],
+                    'nama_sekolah_yang_dibina' => $row[11],
+                    'aktivitas' => $row[12],
                 ]);
             }
         }
