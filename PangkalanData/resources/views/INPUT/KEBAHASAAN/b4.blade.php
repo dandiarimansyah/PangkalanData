8f2a62344e10fc6f6ad20bb5f04089c7ba875dba
Dandi Arimansyah <dandimansyah@gmail.com>
2021-03-08 14:24:04 +0700

    Jumlah Narasumber

--- PangkalanData/resources/views/INPUT/KEBAHASAAN/b4.blade.php
+++ PangkalanData/resources/views/INPUT/KEBAHASAAN/b4.blade.php
@@ -70,10 +70,38 @@
         </div> 
 
         <div class="alert-danger">{{ $errors->first('narasumber') }}</div>
-        <div class="inputfield">
+        <div class="inputfield-kecil">
             <label>Narasumber</label>
-            <input name="narasumber" type="text" class="input">
-        </div> 
+            <label style="width: 110px; margin-right:10px">Jumlah Narasumber</label>
+            <div style="width: 80px" class="custom_select">
+              <select onchange="narasumberFunction()" id="jumlah_narasumber" name="jumlah_narasumber">
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+            <p>*Pilih Jumlah Narasumber</p>
+        </div>
+
+        <div id="parent_narasumber">
+          
+        </div>
+
+        {{-- @for ($i = 1; $i <= 2; $i++)
+          <div class="inputfield">
+            <label></label>
+            <label style="margin-right:10px; margin-left:35px">Narasumber {{$i}}</label>
+            <input name="narasumber{{$i}}" type="text" class="input">
+          </div> 
+        @endfor --}}
 
         <div class="alert-danger">{{ $errors->first('sasaran') }}</div>
         <div class="inputfield">
@@ -114,3 +142,32 @@
 
 </div>
 @endsection
+
+@push('scripts')
+  <script>
+    function narasumberFunction() {
+      var jumlah = document.getElementById("jumlah_narasumber").value;
+      var parent = document.getElementById("parent_narasumber");
+      parent.innerHTML = '';
+
+      for (var i = 1; i <= jumlah; i++) {
+        var div = document.createElement("div");
+        var label1 = document.createElement("label");
+        var label2 = document.createElement("label");
+        var input = document.createElement("input");
+
+        div.className = "inputfield";
+        label2.className = "narasumber_css";
+        label2.innerHTML = "Narasumber " + i;
+        input.className = "input";
+        input.setAttribute('name', 'narasumber'+i);
+
+        div.appendChild(label1);
+        div.appendChild(label2);
+        div.appendChild(input);
+
+        parent.appendChild(div);
+      }
+    }
+  </script>
+@endpush
